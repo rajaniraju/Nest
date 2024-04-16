1e48fea26db4771e117431e6894362d3a0af1bd5
rajaniraju <rajaniraju2003@gmail.com>
2024-04-16 12:57:14 -0500

    Added Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,28 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 export default function App() {
-	return <div className='text-red-500'>App</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+			</Routes>
+			<Routes>
+				<Route path='/about' element={<About />} />
+			</Routes>
+			<Routes>
+				<Route path='/profile' element={<Profile />} />
+			</Routes>
+			<Routes>
+				<Route path='/signIn' element={<SignIn />} />
+			</Routes>
+			<Routes>
+				<Route path='/signUp' element={<SignUp />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
