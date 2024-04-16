546e585eee9c156f00877e5c8d1cce18377e2e88
rajaniraju <rajaniraju2003@gmail.com>
2024-04-16 11:30:03 -0500

    First Commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	/** @type {import('tailwindcss').Config} */
+
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
