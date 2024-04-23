50bb7416e0591c2e422ab6b1d809cfa816f3c0d8
rajaniraju <rajaniraju2003@gmail.com>
2024-04-23 13:35:20 -0500

    Completed image upload functionality.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,98 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import {
+	getDownloadURL,
+	getStorage,
+	ref,
+	uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase.js";
+// firebase coede for rules (inside storage.service firebase.storage {
+/* match /b/{bucket}/o {
+    match /{allPaths=**} {
+      allow read;
+      allow write: if
+      request.resource.size <2*1024*1024 &&
+      request.resource.contentType.matches('image/.*')
+    }
+  }
+}*/
+
+//here useRef helps not to show choosefile / no filechoosen instead when the image is clicked the file should be imported.
 export default function Profile() {
+	const fileRef = useRef(null);
 	const { currentUser } = useSelector((state) => state.user);
+	const [file, setFile] = useState(undefined);
+	const [filepercent, setFilepercent] = useState(0);
+	const [fileUploadError, setFileUploadError] = useState(false);
+	const [formData, setFormData] = useState({});
+	console.log(formData);
+	console.log(filepercent);
+	console.log(fileUploadError);
+	useEffect(() => {
+		if (file) {
+			handleFileUpload(file);
+		}
+	}, [file]);
+	const handleFileUpload = (file) => {
+		const storage = getStorage(app);
+		const fileName = new Date().getTime() + file.name;
+		const storageRef = ref(storage, fileName);
+		const uploadTask = uploadBytesResumable(storageRef, file);
+
+		uploadTask.on(
+			"state_changed",
+			(snapshot) => {
+				const progress =
+					(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+				setFilepercent(Math.round(progress));
+			},
+			(error) => {
+				setFileUploadError(true);
+			},
+			() => {
+				getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+					setFormData({ ...formData, avatar: downloadURL })
+				);
+			}
+		);
+	};
+
 	return (
 		<div className='p-3 max-w-lg mx-auto'>
 			<h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
 			<form className='flex flex-col gap-4'>
+				<input
+					onChange={(e) => {
+						setFile(e.target.files[0]);
+					}}
+					type='file'
+					ref={fileRef}
+					hidden
+					accept='image/*'></input>
 				<img
+					onClick={() => {
+						fileRef.current.click();
+					}}
 					className='rounded-full h-24 w-24  object-cover cursor-pointer self-center mt-2'
-					src={currentUser.avatar}
+					src={formData.avatar || currentUser.avatar}
 					alt='profile'></img>
+				<p className='text-sm self-center'>
+					{fileUploadError ? (
+						<span className='text-red-700'>
+							Error! image was not uploaded (Image must be less than 2mb)
+						</span>
+					) : filepercent > 0 && filepercent < 100 ? (
+						<span className='text-slate-700'>{`Uploading ${filepercent}`}</span>
+					) : filepercent === 100 ? (
+						<span className='text-green-700'>
+							Image successfully Uploaded!
+						</span>
+					) : (
+						" "
+					)}
+				</p>
 				<input
 					type='text'
 					placeholder='username'
