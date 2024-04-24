09573c8d8b0507ae3f17cdead66d7d59b0e28776
rajaniraju <rajaniraju2003@gmail.com>
2024-04-24 10:48:35 -0500

    Completed Delete User Functionality.

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -38,10 +38,32 @@ export const userSlice = createSlice({
 			state.error = action.payload;
 			state.loading = true;
 		},
+		deleteUserStart: (state) => {
+			state.loading = true;
+		},
+		deleteUserSuccess: (state) => {
+			state.currentUser = null;
+			state.loading = false;
+			state.error = null;
+		},
+		deleteUserFailure: (state, action) => {
+			state.error = action.payload;
+			state.loading = true;
+		},
 	},
 });
 
 // Action creators are generated for each case reducer function
-export const { signInStart, signInSucess, signInFailure,updateUserStart,updateUserSuccess,updateUserFailure } = userSlice.actions;
+export const {
+	signInStart,
+	signInSucess,
+	signInFailure,
+	updateUserStart,
+	updateUserSuccess,
+	updateUserFailure,
+	deleteUserStart,
+	deleteUserSuccess,
+	deleteUserFailure,
+} = userSlice.actions;
 
 export default userSlice.reducer;
