bd12c5b7a8f7c5b049bfdbc40bbdfc621f339dda
rajaniraju <rajaniraju2003@gmail.com>
2024-04-24 10:21:12 -0500

    Delete api created

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { test, updateUser } from "../controllers/userController.js";
+import { test, updateUser, deleteUser } from "../controllers/userController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const userRouter = express.Router();
 
 userRouter.get("/test", test);
 userRouter.post("/update/:id", verifyToken, updateUser);
+userRouter.delete("/delete/:id", verifyToken, deleteUser);
 
 export default userRouter;
