6a60d2681437cad80f868d1145bc63b77a1a9d8b
rajaniraju <rajaniraju2003@gmail.com>
2024-04-25 10:00:40 -0500

    Added Upload Functionality.

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -12,19 +12,33 @@ export default function CreateListing() {
 	const [formData, setFormData] = useState({
 		imageUrls: [],
 	});
+	const [imageUploadError, setImageUploadError] = useState(false);
+	const [uploading, setUploading] = useState(false);
 	console.log(formData);
 	const handleImageSubmit = (e) => {
-		if (files.length > 0 && files.length < 7) {
+		if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+			setUploading(true);
+			setImageUploadError(false);
 			const promises = [];
 			for (let i = 0; i < files.length; i++) {
 				promises.push(storeImage(files[i]));
 			}
-			Promise.all(promises).then((urls) => {
-				setFormData({
-					...formData,
-					imageUrls: formData.imageUrls.concat(urls),
+			Promise.all(promises)
+				.then((urls) => {
+					setFormData({
+						...formData,
+						imageUrls: formData.imageUrls.concat(urls),
+					});
+					setImageUploadError(false);
+					setUploading(false);
+				})
+				.catch((err) => {
+					setImageUploadError(`image upload failed!(2mb max per image)`);
+					setUploading(false);
 				});
-			});
+		} else {
+			setImageUploadError(`You can only upload 6 images per listing !`);
+			setUploading(false);
 		}
 	};
 	const storeImage = async (file) => {
@@ -35,6 +49,11 @@ export default function CreateListing() {
 			const uploadTask = uploadBytesResumable(storageRef, file);
 			uploadTask.on(
 				"state_changed",
+				(snapshot) => {
+					const progress =
+						(snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+					console.log(`Upload is ${progress}% done`);
+				},
 				(error) => {
 					reject(error);
 				},
@@ -44,6 +63,12 @@ export default function CreateListing() {
 					});
 				}
 			);
+		});
+	};
+	const handleRemoveImage = (index) => {
+		setFormData({
+			...formData,
+			imageUrls: formData.imageUrls.filter((_, i) => i !== index),
 		});
 	};
 	return (
@@ -177,14 +202,37 @@ export default function CreateListing() {
 						/>
 						<button
 							type='button'
+							disabled={uploading}
 							onClick={handleImageSubmit}
 							className='p-3 text-green-700 border border-green-700 ronded uppercase hover:shadow-lg disabled:opacity-80'>
-							Upload
+							{uploading ? "Uploading..." : "Upload"}
 						</button>
 					</div>
+					<p className='text-red-700 text-sm'>
+						{imageUploadError && imageUploadError}
+					</p>
+
 					<button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80 '>
 						Create Listing
 					</button>
+					{formData.imageUrls.length > 0 &&
+						formData.imageUrls.map((url, index) => (
+							<div
+								key={Math.random()}
+								className='flex justify-between p-3 border items-center'>
+								<img
+									src={url}
+									alt='listing image'
+									className='w-20 h-20 object-contain round-lg'
+								/>
+								<button
+									onClick={() => handleRemoveImage(index)}
+									type='button'
+									className='p-3 text-red-700 rounded-lg uppercase hover:opacity-75'>
+									Delete
+								</button>
+							</div>
+						))}
 				</div>
 			</form>
 		</main>
