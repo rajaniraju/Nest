515316c44814d893ac1084d25cbd4194031224fb
rajaniraju <rajaniraju2003@gmail.com>
2024-04-30 15:54:26 -0500

    Created Search api route

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -4,6 +4,7 @@ import {
 	deleteListing,
 	updateListing,
 	getListing,
+	getListings,
 } from "../controllers/listingController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const listingRouter = express.Router();
@@ -12,4 +13,5 @@ listingRouter.post("/create", verifyToken, createListing);
 listingRouter.delete("/delete/:id", verifyToken, deleteListing);
 listingRouter.post("/update/:id", verifyToken, updateListing);
 listingRouter.get("/get/:id", getListing);
+listingRouter.get("/get", getListings);
 export default listingRouter;
