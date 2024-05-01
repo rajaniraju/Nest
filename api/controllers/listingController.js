7961de87ceca4f972cb6524e7b35128184115937
rajaniraju <rajaniraju2003@gmail.com>
2024-05-01 14:26:38 -0500

    Created Show listings

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -89,6 +89,7 @@ export const getListings = async (req, res, next) => {
 		const searchTerm = req.query.searchTerm || "";
 		const sort = req.query.sort || "createdAt";
 		const order = req.query.order || "desc";
+
 		const listings = await Listing.find({
 			name: { $regex: searchTerm, $options: "i" },
 			offer,
