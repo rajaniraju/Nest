0564eef247b2e76fb6e8cf1ee5ff3c916ea92a59
rajaniraju <rajaniraju2003@gmail.com>
2024-05-02 09:51:18 -0500

    Completed About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
-  return (
-    <div>About</div>
-  )
+	return (
+		<div className='py-20 px-4 max-w-6xl mx-auto'>
+			<h1 className='text-3xl font-bold mb-4 text-slate-800'>About Nest</h1>
+			<p className='mb-4 text-slate-700'>
+				Nest is a leading real estate agency that specializes in helping
+				customers buy, sell, rent properties in the most desirable
+				neighbourhoods. Our team of experienced agents is dedicated to
+				provide exceptional service and making the buying and selling
+				process as smooth as possible.
+			</p>
+			<p className='mb-4 text-slate-700'>
+				Our mission is to help our clients acheive their real estate goals
+				by providing expert advice , personalized service and a deep
+				understanding of the local market. Whether you are looking to buy,
+				sell or rent a property, we ae here to help every step of the way.
+			</p>
+			<p className='mb-4 text-slate-700'>
+				Our team of agents has a wealth of experience and knowledge in the
+				real estate world and we are commited to provide the highest level
+				of service to our clients.
+			</p>
+		</div>
+	);
 }
