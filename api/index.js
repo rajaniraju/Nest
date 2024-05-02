b39c55022421e790d795a842fdf4d09e017ecfb0
rajaniraju <rajaniraju2003@gmail.com>
2024-05-02 11:23:19 -0500

    added preinstallation  commands

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/userRoute.js";
 import authRouter from "./routes/authRoute.js";
 import listingRouter from "./routes/listingRoute.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 mongoose
@@ -19,15 +20,16 @@ app.use(cookieParser());
 app.listen(3000, () => {
 	console.log("server is running on port 3000");
 });
-/*app.get("/test", (req, res) => {
-    res.json({
-        "todays date is ": Date(),
-        message: "get me a cup of cofee!"    });
-});*/
+const __dirname = path.resolve();
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+//build for create react app instead of dist.
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 app.use((err, req, res, next) => {
 	const statusCode = err.statusCode || 500;
 	const message = err.message || "Internal Server Error";
